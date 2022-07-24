2089d0a2dcf42d752dd823a8e2022706449c98d2
Pawel Paczkowski <paw.pac@yahoo.com>
2022-07-24 16:56:08 -0500

    Clean up the code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const addButton = document.getElementById('add-btn');
 const addForm = document.getElementById('add-form');
 const addBookBtn = document.getElementById('add-book-btn');
-const table = document.getElementById('table');
-const tableDiv = document.getElementById('table-div')
+const tableDiv = document.getElementById('table-div');
+const containerForBtn = document.querySelector('.container-for-button');
 
 let bookTitle = document.getElementById('title');
 let bookAuthor = document.getElementById('author');
@@ -10,28 +10,29 @@ let bookPages = document.getElementById('pages');
 let bookStatus = document.getElementById('status');
 
 let bookArray = [];
-let bookLibrary = [];
 
 addButton.addEventListener('click', () => {
   addButton.classList.add('hide');
   addForm.classList.remove('hide');
-
-
-
-
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  bookPages.value = '';
 });
 
 addBookBtn.addEventListener('click', () => {
   addButton.classList.remove('hide');
   addForm.classList.add('hide');
-
   removeChildren(tableDiv);
-
   addBookToLibrary();
   addToDom();
-  console.log(bookLibrary);
-  console.log(bookLibrary[bookLibrary.length - 1]);
+});
 
+containerForBtn.addEventListener('click', (e) => {
+  if (e.target.classList.contains('del')) {
+    bookArray.splice(e.target.id, 1);
+    removeChildren(tableDiv);
+    addToDom();
+  }
 });
 
 function Book(title, author, pages, status) {
@@ -41,29 +42,47 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
+function checkIfChecked(status) {
+  if(status) {
+    return "Yes";
+  } else {
+    return "Not yet"
+  }
+}
+
 function addBookToLibrary() {
-  bookArray.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value));
-  bookLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value));
+  bookArray.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, checkIfChecked(bookStatus.checked)));
 }
 
 function addToDom() {
-
-
-
-
   for (let obj of bookArray) {
-    let newRow = document.createElement('tr');
+    let newRow = document.createElement('div');
+    newRow.classList.add('row');
     newRow.classList.add('newRow' + bookArray.indexOf(obj).toString());
-    newRow.classList.add('test');
+
+    let number = document.createElement('div');
+    number.classList.add('index-delete');
+    number.textContent = (bookArray.indexOf(obj) + 1).toString();
+    newRow.appendChild(number);
     tableDiv.appendChild(newRow);
+
     for (let property in obj) {
-      let newElement = document.createElement('td');
+      let newElement = document.createElement('div');
+      newElement.classList.add('cell');
       newElement.textContent = obj[property];
       let getNewRow = document.querySelector('.newRow' + bookArray.indexOf(obj).toString());
       getNewRow.appendChild(newElement);
     }
+    let delBtnDiv = document.createElement('div');
+    delBtnDiv.classList.add('index-delete');
+
+    let delBtn = document.createElement('button');
+    delBtn.textContent = "X";
+    delBtn.classList.add('del');
+    delBtn.setAttribute('id', bookArray.indexOf(obj).toString());
+    delBtnDiv.appendChild(delBtn);
+    newRow.appendChild(delBtnDiv);
   }
-  // bookArray = [];
 }
 
 function removeClassFromRow() {
@@ -76,10 +95,3 @@ function removeChildren(parent) {
       parent.removeChild(parent.firstChild);
   }
 }
-
-
-
-
-
-
-
